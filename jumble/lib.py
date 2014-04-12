f8ae0d8e575d88c3f4e3af3aafde598347497bc8
Thomas Levine <_@thomaslevine.com>
2014-04-12 16:44:02 +0000

    docstring

--- jumble/lib.py
+++ jumble/lib.py
@@ -1,6 +1,11 @@
 import concurrent.futures
 
 def jumble(func, iterable, max_workers = 50):
+    '''
+    :param func: A function that takes one argument
+    :param iterable: The function will be applied to each element of this iterable.
+    :param max_workers: Because this is running in parallel
+    '''
     jobs = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers) as e:
         for i, item in enumerate(iterable):
