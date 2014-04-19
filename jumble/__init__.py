b5b6d3a6ae9ee0b4a41daaef1118636be82353a3
Thomas Levine <_@thomaslevine.com>
2014-04-19 22:11:29 +0000

    better dependency test

--- jumble/__init__.py
+++ jumble/__init__.py
@@ -1,3 +1,3 @@
 from jumble.lib import jumble
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
 
+try:
+    import concurrent.futures
+except ImportError:
+    install_requires = ['futures==2.1.6'], # python 2 support
+else:
+    install_requires = []
+
 from jumble import __version__
 
 setup(name='jumble',
@@ -8,8 +15,7 @@ setup(name='jumble',
       description='Run something in parallel, and generate results in the order they finish.',
       url='https://github.com/tlevine/jumble',
       packages=['jumble'],
-      install_requires = [],
-      extras_require = ['futures==2.1.6'], # python 2 support
+      install_requires = install_requires,
       tests_require = ['nose'],
       version=__version__,
       license='AGPL',
